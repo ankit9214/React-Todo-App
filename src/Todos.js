4c2f6c1d61c8951123008e7a69bdce2d616ffc8c
Ankit Jain <ankitjain9214@gmail.com>
2019-05-18 16:55:09 +0530

    Initial Commit

---
+++ src/Todos.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Todos = (props) => {
+
+    const todoList = props.todos.length ? (props.todos.map(todo => (
+            <div className = "collection-item" key={todo.id} >
+                <span onClick={() => props.deleteTodo(todo.id)}>{todo.content}</span>
+            </div>
+        )
+    ) ): (
+        <p className="center">You have no todo's left, yay!</p>
+    )
+    
+
+
+
+    return(
+        <div className="todos collection">
+            {todoList}
+        </div>
+    )
+
+}
+
+export default Todos;
